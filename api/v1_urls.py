57f880b663cb9cf144183d7b2b2d381ba5d7fbcc
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2021-02-19 13:29:26 +0300

    Products API endpoints

--- api/v1_urls.py
+++ api/v1_urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from rest_framework import routers
+
+from products.views import ProductViewSet
+
+router = routers.DefaultRouter()
+router.register("products", ProductViewSet)
 
 urlpatterns = []
+
+urlpatterns += router.urls
