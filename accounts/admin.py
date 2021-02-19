8f821aec9cd39d707a71b26fe950251f7bdcbb2e
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2021-02-19 11:25:51 +0300

    Django admin settings for accounts models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Customer
+
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["user", "code", "phone"]
+    list_display_links = ["user", "code"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Customer, CustomerAdmin)
